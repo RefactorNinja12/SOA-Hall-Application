aba1bb7b34bc9a1d384c52ab08c7e718f9262a15
dskotek <tarkovbear@icloud.com>
2025-03-18 11:19:23 +0100

    Fetch API in homecontroller

--- Hall_App/Controllers/HomeController.cs
+++ Hall_App/Controllers/HomeController.cs
@@ -1,20 +1,42 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Hall_App.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hall_App.Controllers
 {
+
+    public class GetAPI
+    {
+        private readonly HttpClient _httpClient;
+
+        public GetAPI(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<ArcadeHall>> GetAPIAsync()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7234/api/ArcadeHall");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<ArcadeHall>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+            return null;
+        }
+    }
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        private readonly GetAPI _getAPI;
 
-        public IActionResult Arcadehalls()
+        public HomeController(GetAPI getAPI)
         {
-            return View();
+            _getAPI = getAPI;
         }
-
+        public async Task<IActionResult> Index()
+        {
+            var data = await _getAPI.GetAPIAsync();
+            return View(data);
+        }        
     }
 }
--- Hall_App/Program.cs
+++ Hall_App/Program.cs
@@ -1,7 +1,11 @@
+using Hall_App.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<GetAPI>();
 
 var app = builder.Build();
 
@@ -22,7 +26,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Arcadehalls}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
