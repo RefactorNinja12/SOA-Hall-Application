ff0eb520221e00b3c8275660fa02860be190af63
RefactorNinja12 <farfromhorizon91gmail.com>
2025-03-22 14:00:01 +0100

    Added new changes to input field and tabell

--- Hall_App/Models/ArcadeHall.cs
+++ Hall_App/Models/ArcadeHall.cs
@@ -1,11 +1,20 @@
-﻿namespace Hall_App.Models
+﻿using Hall_App.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hall_App.Models
 {
     public class ArcadeHall
     {
+        
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
-        public string ImageUrl { get; set; } = string.Empty;
+        public string ImageURL { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public int MaxCapacity { get; set; }
+        public HallAvailability HallStatus { get; set; }
+
 
     }
 }
